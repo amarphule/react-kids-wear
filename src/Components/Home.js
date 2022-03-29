9ba40d5c61ae9fd1129ace3c25f676657f040e9b
Amardeep Phule <amardeepphule@gmail.com>
2022-03-29 21:57:44 +0530

    demo link

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 function Home() {
   return (
-    <div>Home</div>
+    
+    <div>
+      <Link to="/cart">cart</Link>
+      </div>
   )
 }
 
