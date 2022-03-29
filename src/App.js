9cf3b5fd2a77589d9bb4424b36075ccf327bb384
Amardeep Phule <amardeepphule@gmail.com>
2022-03-29 21:28:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import logo from "./logo.png";
+import Cart from "./Components/Cart";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import ProductLists from "./Components/ProductLists";
+import Signup from "./Components/Signup";
+import WishList from "./Components/WishList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
+     <h1>Hello ecom</h1>
+     <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/login" element={<Login/>}/>
+       <Route path="/signup" element={<Signup/>}/>
+       <Route path="/product-lists" element={<ProductLists/>}/>
+       <Route path="/wishlist" element={<WishList/>}/>
+       <Route path="/cart" element={<Cart/>}/>
+     </Routes>
+     
+     
+     
+     
+     
+     
     </div>
   );
 }
---
+++ src/Components/ProductLists.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductLists = () => {
+  return (
+    <div>ProductLists</div>
+  )
+}
+
+export default ProductLists
