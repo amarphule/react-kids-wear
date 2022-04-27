deb8355aa7980ac5f6d0927138e85062d56df863
Amardeep Phule <amardeepphule@gmail.com>
2022-04-27 15:44:37 +0530

    feat: Guest login

--- src/Contexts/ProductContext.js
+++ src/Contexts/ProductContext.js
@@ -1,29 +1,30 @@
-import React,{ createContext, useState, useContext,useEffect } from 'react'
-import axios from 'axios'
+import React, { createContext, useState, useContext, useEffect } from "react";
+import axios from "axios";
 
-const ProductContext = createContext()
+const ProductContext = createContext();
 
-const ProductProvider = ({children}) => {
+const ProductProvider = ({ children }) => {
+  const [productLists, setProductLists] = useState([]);
 
-    const [productLists, setProductLists] = useState([])
-
-    useEffect(()=>{
-        const getProducts = async ()=>{
-            try {
-                const resp = await axios("/api/products")
-                setProductLists(resp.data.products)
-            } catch (error) {
-            console.log("log: ",error)     
-            }
-        }
-        getProducts()
-    },[])
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const resp = await axios("/api/products");
+        setProductLists(resp.data.products);
+      } catch (error) {
+        console.log("log: ", error);
+      }
+    };
+    getProducts();
+  }, []);
 
   return (
-    <ProductContext.Provider value={{productLists, setProductLists}}>{children}</ProductContext.Provider>
-  )
-}
+    <ProductContext.Provider value={{ productLists, setProductLists }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
 
-const useProductLists = ()=>useContext(ProductContext)
+const useProductLists = () => useContext(ProductContext);
 
-export {ProductProvider, useProductLists} 
+export { ProductProvider, useProductLists };
